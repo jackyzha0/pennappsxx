01fb05eaed4d39ff5d157407da9494ee9fe477cc
John Lee <jlee14c@hotmail.com>
2019-09-07 14:14:43 -0400

    ui fixes

--- pythonServer/test.sh
+++ pythonServer/test.sh
@@ -1,12 +1,12 @@
+# curl -X POST -H "Content-Type: application/json" \
+#  -d '{"command":"commando"}' \
+#  http://localhost:8000/command
+
 curl -X POST -H "Content-Type: application/json" \
   -d '{"command":"commando"}' \
-  http://localhost:8000/command
+  https://pennappsxx.herokuapp.com/command
 
-#curl -X POST -H "Content-Type: application/json" \
-#  -d '{"command":"commando"}' \
-#  https://pennappsxx.herokuapp.com/command
-
-# curl -X GET https://pennappsxx.herokuapp.com/fetch
+curl -X GET https://pennappsxx.herokuapp.com/fetch
 
 
 
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,88 +1,53 @@
-import React from 'react';
-import pic from './debris.jpg';
 import './App.css';
-import SideBar from './sidebar';
 import 'typeface-roboto';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
+import React from 'react';
+import history from './history';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import SearchIcon from '@material-ui/icons/Search';
-import HomeIcon from '@material-ui/icons/Home';
-import MapIcon from '@material-ui/icons/Map';
+import NavBar from './NavBar/NavBar';
+import PlanScreen from './PlanScreen/PlanScreen';
+import ImageScreen from './ImageScreen/ImageScreen';
+import DataScreen from './DataScreen/DataScreen';
 
-import { makeStyles } from '@material-ui/core/styles';
-
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles(theme => ({
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  toolbar: theme.mixins.toolbar,
-}));
-
-export default function WebApp() {
-
-  const classes = useStyles();
-
-  return(
-      <div className="App">
-        <Drawer
-          className={classes.drawer}
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          anchor="left"
-        >
-          <div className={classes.toolbar} />
-          <Divider />
-          <List>
-            {['Home', 'Starred'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <HomeIcon /> : <SearchIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
-          <List>
-            {['Map'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <MapIcon /> : <SearchIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-        <Grid
-          container
-          direction="column"
-          justify="center"
-          alignItems="center"
-        >
-          <img src={pic} width="500" height="300" />
-          <Box component="span" m={5}>
-            <Button variant="contained" color="primary" size="large">
-              Deploy
-            </Button>
-          </Box>
-        </Grid>
-      </div>
-    );
+const styles = {
+  container: {
+    display: 'flex',
+    width: '100%',
+    height: '100%'
+  }
 }
+
+const WebApp = ({}) => {
+  return (
+    <div className="App">
+      <Router history={history}>
+        <div style={styles.container}>
+          <NavBar/>
+          <Route
+            render={({ location }) => (
+              <Switch location={location}>
+                <Route
+                  exact
+                  path="/"
+                  component={PlanScreen}
+                />
+                <Route
+                  exact
+                  path="/images"
+                  component={ImageScreen}
+                />
+                <Route
+                  exact
+                  path="/data"
+                  component={DataScreen}
+                />
+              </Switch>
+            )}
+          />
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default WebApp;
