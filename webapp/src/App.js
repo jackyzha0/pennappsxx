783a8716e646da5747f3b17321d7092aa0b69f1c
Colin Zhou <colinzhou@berkeley.edu>
2019-09-06 23:14:36 -0400

    Added Input and Sidebar

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import pic from './debris.jpg';
 import './App.css';
+import SideBar from './sidebar';
 
 export default class MarineDebris extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {
 
-    }
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+
+
+    event.preventDefault();
   }
 
   render() {
     return(
     <div className="App">
+      <SideBar />
       <header className="App-header">
         <p>
           Marine Debris Locator
@@ -25,6 +39,13 @@ export default class MarineDebris extends React.Component {
         <button>
           Process
         </button>
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Command:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
       </header>
     </div>
     );
