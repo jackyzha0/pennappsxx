4ea618dbf246c276a2ff6d6444a2408f8629059a
John Lee <jlee14c@hotmail.com>
2019-09-07 09:25:13 -0400

    updated commands

--- pythonServer/test.sh
+++ pythonServer/test.sh
@@ -1,8 +1,11 @@
-curl -X POST -H "Content-Type: application/json" \
-  -d '{"command":"commando"}' \
-  http://localhost:8000/command
-
 # curl -X POST -H "Content-Type: application/json" \
-# -d '{"command":"commando"}' \
+#   -d '{"command":"commando"}' \
+#   http://localhost:8000/command
+
+#curl -X POST -H "Content-Type: application/json" \
+#  -d '{"command":"commando"}' \
 #  https://pennappsxx.herokuapp.com/command
+
+curl -X GET https://pennappsxx.herokuapp.com/fetch
+
 
