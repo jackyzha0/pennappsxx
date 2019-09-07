88a6f8cfdf7f8e866acb18ea7e8c019a72647ad2
unknown <jacky.zhao2019@stgeorges.bc.ca>
2019-09-07 06:24:14 -0700

    repush

--- drone_control/swarm.py
+++ drone_control/swarm.py
@@ -1,32 +1,28 @@
 from djitellopy import TelloSwarm
-import time
 import signal
-import sys
 
 def run():
     ips = [x.rstrip() for x in open('ips.txt', 'r').readlines()][1:-1]
     ips = [x[0:20].replace(" ","") for x in ips]
     # ips = ["192.168.137.156"]
     swarm = TelloSwarm.fromIps(ips)
-
     swarm.connect()
-    # swarm.takeoff()
-
-    # run in parallel on all tellos
+    swarm.send_read_command("battery?")
     swarm.send_command_with_return("takeoff")
+    swarm.sync(3)
 
     swarm.send_command_with_return("up 50")
-    time.sleep(3)
-    # swarm.send_command_without_return("forward 50")
-    # time.sleep(3)
+    swarm.sync(3)
+    swarm.send_command_with_return("forward 50")
+    swarm.sync(3)
     # # run by one tello after the other
     # swarm.sequential(lambda i, tello: tello.move_forward(i * 20))
     #
     # # making each tello do something unique in parallel
     # swarm.parallel(lambda i, tello: tello.move_left(i * 100))
-    swarm.get_frame_read().frame
+    # swarm.get_frame_read().frame
 
-    swarm.send_command_with_return("land")
+    swarm.land()
     swarm.end()
 
 
@@ -38,8 +34,7 @@ def exit_gracefully(signum, frame):
     ips = [x.rstrip() for x in open('ips.txt', 'r').readlines()][1:-1]
     ips = [x[0:20].replace(" ","") for x in ips]
     swarm = TelloSwarm.fromIps(ips)
-    swarm.connect()
-    swarm.send_command_with_return("land")
+    swarm.land()
     swarm.end()
 
     # restore the exit gracefully handler here
