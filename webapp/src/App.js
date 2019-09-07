8e8bce1820711213ec2b7ae00fcd1a2c6aee85d9
Colin Zhou <colinzhou@berkeley.edu>
2019-09-07 10:59:01 -0400

    Added materialui

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -2,83 +2,87 @@ import React from 'react';
 import pic from './debris.jpg';
 import './App.css';
 import SideBar from './sidebar';
-import io from 'socket.io-client';
-// const io = require('socket.io-client');
+import 'typeface-roboto';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
-const PYTHON_SERVER_URL = 'https://pennappsxx-server.herokuapp.com:8000';
-let socket;
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import MapIcon from '@material-ui/icons/Map';
 
-export default class MarineDebris extends React.Component {
+import { makeStyles } from '@material-ui/core/styles';
 
-  constructor(props) {
-    super(props)
 
-    this.state = {
-      connected: false,
-      value: ''
-    };
+const drawerWidth = 240;
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const useStyles = makeStyles(theme => ({
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+}));
 
-  componentWillMount() {
-    socket = io(PYTHON_SERVER_URL);
-    console.log('here')
-    socket.on('update', (data) => {
-      console.log('update')
-      this.setState({ data, connected: true })
-    });
+export default function WebApp() {
 
-    socket.on('disconnect', () => {
-      console.log('disconnect')
-      this.setState({ connected: false })
-    });
+  const classes = useStyles();
 
-    socket.on('reconnect', () => {
-      console.log('reconnect')
-      this.setState({ connected: true })
-    });
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    console.log('submit')
-
-    socket.emit('update', {
-      data: 'User Connected'
-    } )
-
-    event.preventDefault();
-  }
-
-  render() {
-    return(
-    <div className="App">
-      <SideBar />
-      <header className="App-header">
-        <p>
-          Marine Debris Locator
-        </p>
-        <img src={pic} width="500" height="300" />
-        <button>
-          Map
-        </button>
-        <button>
-          Process
-        </button>
-        <form onSubmit={this.handleSubmit}>
-        <label>
-          Command:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      </header>
-    </div>
+  return(
+      <div className="App">
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          anchor="left"
+        >
+          <div className={classes.toolbar} />
+          <Divider />
+          <List>
+            {['Home', 'Starred'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <HomeIcon /> : <SearchIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['Map'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index % 2 === 0 ? <MapIcon /> : <SearchIcon />}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <img src={pic} width="500" height="300" />
+          <Box component="span" m={5}>
+            <Button variant="contained" color="primary" size="large">
+              Deploy
+            </Button>
+          </Box>
+        </Grid>
+      </div>
     );
-  }
 }
