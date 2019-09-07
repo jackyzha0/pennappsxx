d1bc3df9d9fa87bfabf618ae4dbf57f70d0201fb
John Lee <jlee14c@hotmail.com>
2019-09-07 00:55:32 -0400

    wip

---
+++ pythonServer/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_restplus import Api, Resource
+
+from . import api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Status(Resource):
+    def get(self):
+        return {
+            "status": "Up and running"
+        }, 200
+
+class Command(Resource):
+    def post(self):
+        json_data = request.get_json(force=True)
+        cmd = json_data['command']
+        print("command is: ", cmd)
+
+        # TODO: forward this command to the drone proxy
+        return {
+            "result": "OK"
+        }, 200
+
+api.add_resource(Status, '/status')
+api.add_resource(Command, '/command')
+
+if __name__ == "__main__":
+  app.run()
